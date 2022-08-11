c1c342a6ab1c771a5feabdbe2c19f3b8a8013f51
bt0mx <104509348+bt0mx@users.noreply.github.com>
2022-08-10 20:20:09 -0500

    Add files via upload

---
+++ validacion.js
@@ -0,0 +1,9 @@
+//Haz tú validación en javascript acá
+function Private(){
+    alert("Este repositorio es privado");
+}
+
+function Message(){
+    alert("Tu mensaje a sido enviado exitosamente, en breve me comunico contigo");
+}
+
